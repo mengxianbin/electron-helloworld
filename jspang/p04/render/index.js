1202149112f4d88b659628f3b97288dd513e43eb
mengxianbin <mengxianbindev@gmail.com>
2020-12-12 03:27:43 +0800

    feat(jspang p04): update

--- jspang/p04/render/index.js
+++ jspang/p04/render/index.js
@@ -1,10 +1,10 @@
 var fs = require('fs')
 window.onload = function () {
-    var btn = this.document.querySelector('#btn')
-    var text = this.document.querySelector('#text')
-    btn.onclick = function () {
-        fs.readFile('text.txt', (err, data) => {
-            text.innerHTML = data
+    var load = this.document.querySelector('#load')
+    var content = this.document.querySelector('#content')
+    load.onclick = function () {
+        fs.readFile('data', (err, data) => {
+            content.innerHTML = data
             console.log(err)
         })
     }
