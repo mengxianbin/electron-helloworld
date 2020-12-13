4f4e285a0178bd33796cd402c33cca29c54dddd8
mengxianbin <mengxianbindev@gmail.com>
2020-12-13 19:08:11 +0800

    feat(jspang p12): save dialog

---
+++ jspang/p12-savedialoog/main.js
@@ -0,0 +1,11 @@
+const { app, BrowserWindow } = require('electron')
+app.whenReady().then(() => {
+    new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true
+        }
+    }).loadFile('index.html')
+}).catch(error => {
+    alert(error)
+})
